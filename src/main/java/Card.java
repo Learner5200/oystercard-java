befe0d0ac41a32dc0434d8906df18d5fffbd3969
chriscooksley <christopher.cooksley@gmail.com>
2019-03-12 09:45:11 +0000

    No one will ever read this

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,8 +1,45 @@
+import java.util.ArrayList;
+
 class Card {
     private Account account;
+    private JourneyLog journeyLog;
+    private Journey currentJourney;
 
-    Card(Account account) {
+    Card(Account account, JourneyLog journeyLog) {
         this.account = account;
+        this.journeyLog = journeyLog;
+    }
+
+    int getBalance() {
+        return this.account.getBalance();
+    }
+
+    ArrayList<Journey> getJourneyLog() {
+        return this.journeyLog.getJourneys();
+    }
+
+    void topUp(int amount) throws Exception {
+        this.account.topUp(amount);
+    }
+
+    void spend(int amount) throws Exception {
+        this.account.spend(amount);
+    }
+
+    Journey getCurrentJourney() {
+        return this.currentJourney;
+    }
+
+    void setCurrentJourney(Journey journey) {
+        this.currentJourney = journey;
+    }
+
+    void touchIn(Station station) {
+        setCurrentJourney(new Journey(station));
+    }
+
+    void touchOut(Station station) {
+        currentJourney.exit(station);
     }
 
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,18 +1,77 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class CardTest {
+    Account mockAccount;
+    JourneyLog mockJourneyLog;
+    Card card;
 
     @Before
     public void setUp() throws Exception {
-        Account mockAccount = mock(Account.class);
-        Card card = new Card(mockAccount);
+        mockAccount = mock(Account.class);
+        mockJourneyLog = mock(JourneyLog.class);
+        card = new Card(mockAccount, mockJourneyLog);
+    }
+
+
+    @Test
+    public void canDisplayBalanceFromAccount() {
+        when(mockAccount.getBalance()).thenReturn(20);
+        assertEquals(20, card.getBalance());
     }
 
     @Test
-    public void name() {
+    public void canTopUpAccount() throws Exception {
+        card.topUp(1);
+        verify(mockAccount).topUp(1);
+    }
+
+    @Test
+    public void canSpendFromAccount() throws Exception {
+        card.spend(1);
+        verify(mockAccount).spend(1);
+    }
+
+    @Test
+    public void canDisplayJourneyLog() {
+        Journey mockJourney1 = mock(Journey.class);
+        Journey mockJourney2 = mock(Journey.class);
+        ArrayList<Journey> mockLog = new ArrayList<Journey>(Arrays.asList(mockJourney1, mockJourney2));
+        when(mockJourneyLog.getJourneys()).thenReturn(mockLog);
+        assertEquals(mockLog, card.getJourneyLog());
+    }
+
+    @Test
+    public void touchingInCreatesNewJourney() {
+        Station mockStation1 = mock(Station.class);
+        card.touchIn(mockStation1);
+        Journey currentJourney = card.getCurrentJourney();
+        assertEquals(mockStation1, currentJourney.getEntryStation());
+    }
+
+    @Test
+    public void touchingOutUpdatesCurrentJourney() {
+//        Station mockStation1 = mock(Station.class);
+//        Station mockStation2 = mock(Station.class);
+//        card.touchIn(mockStation1);
+//        Journey currentJourneySpy = spy(card.getCurrentJourney());
+//        card.touchOut(mockStation2);
+//        verify(currentJourneySpy).exit(mockStation2);
+    }
+
+    @Test
+    public void touchingOutSpendsFare() {
+        Station mockStation1 = mock(Station.class);
+        Station mockStation2 = mock(Station.class);
+        card.touchIn(mockStation1);
+        card.touchOut(mockStation2);
+        int fare = card.getCurrentJourney().calculateFare();
+
     }
 }
